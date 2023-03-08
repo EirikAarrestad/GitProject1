159781ed159aad4074845d0ad2fcf9c67822278a
= <=>
2023-03-08 21:46:12 +0100

    fixed javascript

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-//Declaring variables
+// Get references to the HTML elements we need
 const inputNumberOne = document.querySelector("#inputNr1");
 const inputNumberTwo = document.querySelector("#inputNr2");
-
 const output = document.querySelector("#output");
 const operator = document.querySelector("#operatorSelect");
-
 const calculateButton = document.querySelector("#button");
 
+// Add a click event listener to the calculate button
 calculateButton.addEventListener("click", () => {
-    // create variables for the numbers
+    // Get the values of the input numbers
     var numberOne = Number(inputNumberOne.value);
     var numberTwo = Number(inputNumberTwo.value);
 
-    console.log(numberOne + numberTwo);
+    // Determine which operator was selected
+    var operatorType = operator.value;
+
+    // Calculate the result based on the selected operator
+    var result =
+        operatorType == "addition"
+            ? numberOne + numberTwo
+            : numberOne - numberTwo;
+
+    // Display the result in the output element
+    output.textContent = result;
 });
